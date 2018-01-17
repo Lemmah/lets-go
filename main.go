0d3335beb5447acee7b3093f9a59ac366a002a9d
Lemmah <lemayiannakolah@gmail.com>
2018-01-17 23:58:21 +0300

    feat(stdio): Get input from stdio.

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
-import "fmt"
 
-func main ()  {
+import (
+	"fmt"
+)
+
+func main() {
+
 	fmt.Println("Hello, Let's Go!!")
+
+	var option int
+
+	fmt.Scanln(&option)
+
+	fmt.Println(option)
 }
