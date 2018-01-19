f6abd38764814d51f9e33fbd81c2f8141cfad35b
Lemmah <lemayiannakolah@gmail.com>
2018-01-19 18:14:52 +0300

    learn(concept): Working with Slices instead of arrays
    Slices are extendible unlike arrays which have fixed length and cannot be extended past the length.

--- main.go
+++ main.go
@@ -5,5 +5,8 @@ import "fmt"
 func main() {
 	/* Collections in Go */
 	myArray := [...]int{42, 43, 44, 45}
+	mySlice := myArray[:]
+	mySlice = append(mySlice, 46)
 	fmt.Println(myArray)
+	fmt.Println(mySlice)
 }
