364aff8c529a3caac9fcb8bbe8c48402577f99ec
Lemmah <lemayiannakolah@gmail.com>
2018-01-19 17:23:26 +0300

    learn(concept): DataTypes in Go.

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-
-	fmt.Println("Hello, Let's Go!!")
-
-	var option int
-
-	fmt.Scanln(&option)
-
-	fmt.Println(option)
+	/* variable datatypes in go */
+	var myInt int
+	myInt = 43
+	println(myInt)
+	secondInt := 44 // the shorthand version
+	println(secondInt)
+	var myFloat float32 = 32.
+	println(myFloat)
+	myFloatTwo := 32.
+	println(myFloatTwo)
+	myString := "Hello Go!"
+	println(myString)
+	myComplex := complex(3, 2)
+	println(myComplex)
+	println(real(myComplex))
+	println(imag(myComplex))
 }
