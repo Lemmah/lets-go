793b79462095519ef4734169b43f13297b0a2fbc
Lemmah <lemayiannakolah@gmail.com>
2018-01-19 18:24:58 +0300

    learn(concept): Working with Maps
    Maps in go are the equal of dictionaries in python.

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	/* Collections in Go */
-	mySlice := make([]int, 100)
-	mySlice[0] = 43
-	mySlice[1] = 44
-	mySlice[2] = 45
-	mySlice = append(mySlice, 46)
-	fmt.Println(mySlice)
+	/* maps, dictionaries in go */
+	myMap := make(map[int]string)
+	myMap[1] = "Lemmah"
+	myMap[2] = "James"
+	fmt.Println(myMap)
 }
