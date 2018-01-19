c418460ff235c15758d6bcfc23bb99aaf2c66e46
Lemmah <lemayiannakolah@gmail.com>
2018-01-19 23:01:17 +0300

    feat(simple-for-loop): Simple for loop in go

--- main.go
+++ main.go
@@ -3,13 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var plantCapacities []float64
-	plantCapacities = []float64{30, 30, 30, 60, 60, 100}
-	var capacity = plantCapacities[0] + plantCapacities[1] + plantCapacities[2] + plantCapacities[3] + plantCapacities[4] + plantCapacities[5]
-	var gridLoad = 75.
-	utilization := gridLoad / capacity
-	// Printf need *formatting patterns*
-	fmt.Printf("%-20s%.0f\n", "Capacity:", capacity)
-	fmt.Printf("%-20s%.0f\n", "Grid Load:", gridLoad)
-	fmt.Printf("%-20s%.1f%%\n", "Utilization:", utilization*100)
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+	}
 }
