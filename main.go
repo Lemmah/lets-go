ca1e1f3b497f4fc0af5f358b33b812f5071e6e5e
Lemmah <lemayiannakolah@gmail.com>
2018-01-19 18:16:51 +0300

    learn(concept): Shorthand declaration for slices.
    You do not need a parent array to create a slice. This is weird and awesome at the same time.

--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import "fmt"
 
 func main() {
 	/* Collections in Go */
-	myArray := [...]int{42, 43, 44, 45}
-	mySlice := myArray[:]
+	mySlice := []int{42, 43, 44, 45}
 	mySlice = append(mySlice, 46)
-	fmt.Println(myArray)
 	fmt.Println(mySlice)
 }
