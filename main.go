4c3ca6e9f83219b5887d883198506860e27c9aa8
Lemmah <lemayiannakolah@gmail.com>
2018-01-19 17:52:32 +0300

    learn(concept): Advance Computation with iota.

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 /* Constants in Go */
 const (
-	first = iota
+	first = 1 << (1 * iota)
 	second
 	third
+	fourth
+	fifth
+	sixth
+	seventh
+	eighth
+	nineth
+	tenth
 )
 
 func main() {
 	println(first)
 	println(second)
 	println(third)
+	println(fourth)
+	println(fifth)
+	println(sixth)
+	println(seventh)
+	println(eighth)
+	println(nineth)
+	println(tenth)
 }
