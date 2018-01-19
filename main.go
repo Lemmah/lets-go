3d44ba81b80baf4b0c5437254a9ca8b87b6bf49a
Lemmah <lemayiannakolah@gmail.com>
2018-01-20 00:21:20 +0300

    feat(simple-go-function): A simple Go function.

--- main.go
+++ main.go
@@ -3,13 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var plantCapacities []float64
-	plantCapacities = []float64{30, 30, 30, 60, 60, 100}
-	var capacity = plantCapacities[0] + plantCapacities[1] + plantCapacities[2] + plantCapacities[3] + plantCapacities[4] + plantCapacities[5]
-	var gridLoad = 75.
-	utilization := gridLoad / capacity
-	// Printf need *formatting patterns*
-	fmt.Printf("%-20s%.0f\n", "Capacity:", capacity)
-	fmt.Printf("%-20s%.0f\n", "Grid Load:", gridLoad)
-	fmt.Printf("%-20s%.1f%%\n", "Utilization:", utilization*100)
+	sayHello("Lemmah")
+}
+
+func sayHello(name string) {
+	fmt.Printf("Hello %s\n", name)
 }
