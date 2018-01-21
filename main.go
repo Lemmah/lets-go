c2a67f158133a38d7aca91ecd4380a225f19ee56
Lemmah <lemayiannakolah@gmail.com>
2018-01-22 00:36:20 +0300

    concept(structs): Create Object Refrence type on Heap
    The second way to create object refrence types on heaps.

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	// It's recommended to create objects on the heap
+	// On the heap, we refer to a refrence type
 	foo := new(myStruct)
 	foo.myField = "bar"
 	println(foo.myField)
+	// Confirm that it's a refrence type
+	fmt.Println(foo)
+	bar := &myStruct{"foo"}
+	fmt.Println(bar)
 }
 
 /* Struct is a custom type */
