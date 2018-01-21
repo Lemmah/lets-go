26793b512df8ffc3cce56d5d5beb40349460e87d
Lemmah <lemayiannakolah@gmail.com>
2018-01-22 00:24:20 +0300

    concept(structs): Create a simple type struct
    Structs are used to create object in go. It's no that they are classes. They are something much more different.

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
 func main() {
+	// Creating an object in the local memory stac
+	foo := myStruct{}
+	foo.myField = "bar"
+	println(foo.myField)
+}
 
+/* Struct is a custom type */
+type myStruct struct {
+	myField string
 }
