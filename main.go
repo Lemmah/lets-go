066418b6e771dc52738d27ee358a837df2f01568
Lemmah <lemayiannakolah@gmail.com>
2018-01-21 23:39:28 +0300

    Use anonymous functions

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var plantCapacities []float64
-	plantCapacities = []float64{30, 30, 30, 60, 60, 100}
-	var capacity = plantCapacities[0] + plantCapacities[1] + plantCapacities[2] + plantCapacities[3] + plantCapacities[4] + plantCapacities[5]
-	var gridLoad = 75.
-	utilization := gridLoad / capacity
-	// Printf need *formatting patterns*
-	fmt.Printf("%-20s%.0f\n", "Capacity:", capacity)
-	fmt.Printf("%-20s%.0f\n", "Grid Load:", gridLoad)
-	fmt.Printf("%-20s%.1f%%\n", "Utilization:", utilization*100)
+	addFunc := func(terms ...int) (numTerms int, sum int) {
+		sum = 0
+		for _, term := range terms {
+			sum += term
+		}
+		numTerms = len(terms)
+		return
+	}
+	fmt.Println(addFunc(2, 3, 4))
 }
