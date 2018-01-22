93dee62c79de42eed37e557cfed36a16e01750c6
Lemmah <lemayiannakolah@gmail.com>
2018-01-22 09:50:29 +0300

    concept(structs): Constructor funcs

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	foo := newMyStruct()
+	foo.myMap["foo"] = "bar"
+	fmt.Println(foo)
+}
+
+type myStruct struct {
+	myMap map[string]string
+}
+
+func newMyStruct() *myStruct {
+	result := myStruct{}
+	result.myMap = map[string]string{}
+	return &result
 }
