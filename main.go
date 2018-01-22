8bd04196213652ed10f8408f5cc5f436af14fa6e
Lemmah <lemayiannakolah@gmail.com>
2018-01-22 10:01:31 +0300

    concept(methods): Create a simple go method.

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	mp := messagePrinter{"This is the message"}
+	mp.printMessage()
+}
+
+type messagePrinter struct {
+	message string
+}
+
+// A method of the message printer
+func (mp *messagePrinter) printMessage() {
+	fmt.Println(mp.message)
 }
