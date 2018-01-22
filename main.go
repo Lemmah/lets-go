b5c4dd3cc6082cc863e55b7ee88a2ca279b43382
Lemmah <lemayiannakolah@gmail.com>
2018-01-22 10:08:41 +0300

    concept(object-composition): Implement Object Composition
    Object composition is go's way of inheritance. It's not inheritance just as structs are not objects but it gives go most  of the iheritance capabilities.

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	mp := messagePrinter{"This is the message"}
+	mp := enhancedMessagePrinter{}
+	mp.message = "This is the message"
 	mp.printMessage()
 }
 
@@ -14,4 +15,9 @@ type messagePrinter struct {
 // A method of the message printer
 func (mp *messagePrinter) printMessage() {
 	fmt.Println(mp.message)
+}
+
+// Object composition is go's replacement of inheritance
+type enhancedMessagePrinter struct {
+	messagePrinter
 }
